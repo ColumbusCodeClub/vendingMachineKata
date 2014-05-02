663084c0f00a2ad19e06805b0f789363d80e4ab3
scoobycoder <scoobycoder@gmail.com>
2014-05-02 16:17:08 -0400

    adding coin changes from second day of code club, coins have values now

--- src/vending/Coin.java
+++ src/vending/Coin.java
@@ -1,4 +1,11 @@
 
 public enum Coin {
-	QUARTER, DIME, NICKEL, PENNY;
+	QUARTER(0.25), DIME(0.1), NICKEL(0.05), PENNY(0.01);
+	private double value;
+	Coin(double value) {
+		this.value = value;
+	}
+	public Double getValue() {
+		return value;
+	}
 }
